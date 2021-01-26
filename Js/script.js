d885a639ba00bbf5ffbe7e528aa304642221ce4e
JonatasAR1 <jonatasar01@hotmail.com>
2021-01-26 16:35:38 -0300

    Projeto finalizado

---
+++ Js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.menu-item').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html,body').animate({'scrollTop':offSetTop});
+
+        return false;
+    });
+});
